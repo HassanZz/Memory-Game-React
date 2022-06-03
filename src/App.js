0a4a6523428995621ba77b488121fcb8881f9525
HassanZz <hassan.zamani.75@gmail.com>
2022-06-04 00:05:47 +0430

    compare two cards and flip them if matched added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 function App() {
   const images = [
-    { src: "/img/accept-man.PNG" },
-    { src: "/img/bestWay-man.jpg" },
-    { src: "/img/cat.png" },
-    { src: "/img/crying-cat.jpg" },
-    { src: "/img/denying man.PNG" },
-    { src: "/img/Parviz.jpeg" },
+    { src: "/img/accept-man.PNG", matched: false },
+    { src: "/img/bestWay-man.jpg", matched: false },
+    { src: "/img/cat.png", matched: false },
+    { src: "/img/crying-cat.jpg", matched: false },
+    { src: "/img/denying man.PNG", matched: false },
+    { src: "/img/Parviz.jpeg", matched: false },
   ];
 
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiseOne, setChoiseOne] = useState(null);
+  const [choiseTwo, setChoiseTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -23,8 +25,35 @@ function App() {
     setCards(shuffledCards);
     setTurns(0);
   };
+  const handleClick = (card) => {
+    !choiseOne ? setChoiseOne(card) : setChoiseTwo(card);
+  };
 
-  console.log(cards, turns);
+  const resetTurn = () => {
+    setChoiseOne(null);
+    setChoiseTwo(null);
+    setTurns((prevTurn) => prevTurn + 1);
+  };
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiseOne) {
+      if (choiseOne.src === choiseTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiseOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+      }
+
+      resetTurn();
+    }
+  }, [choiseTwo]);
+
+  console.log(cards);
 
   return (
     <div className="App ">
@@ -35,7 +64,12 @@ function App() {
 
       <div className="grid grid-cols-4 gap-2">
         {cards.map((item) => (
-          <SingleCard item={item} key={item.id} />
+          <SingleCard
+            key={item.id}
+            item={item}
+            handleClick={handleClick}
+            flipped={item === choiseOne || item === choiseTwo || item.matched}
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,16 +1,15 @@
-import { useState } from "react";
 import "./SingleCard.css";
 
-const MakeGridCards = ({ item }) => {
-  const [showCard, setShowCard] = useState(true);
+const MakeGridCards = ({ item, handleClick, flipped }) => {
   return (
     <div
       key={item.id}
       className=" cursor-pointer hover:opacity-80 hover:scale-105 border-white border-2 rounded-md"
-      onClick={() => setShowCard(item.id)}
     >
-      {false && <img src={item.src}></img>}
-      {<img src="/img/cover.jpg"></img>}
+      {flipped && <img src={item.src}></img>}
+      {!flipped && (
+        <img src="/img/cover.jpg" onClick={() => handleClick(item)}></img>
+      )}
     </div>
   );
 };
