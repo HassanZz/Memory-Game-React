845d403f87daf9b69c57b052752e63857f4d809b
HassanZz <hassan.zamani.75@gmail.com>
2022-06-03 18:25:50 +0430

    shuffleCard functino added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import "./App.css";
-
 function App() {
+  const images = [
+    { src: "/img/accept-man.PNG" },
+    { src: "/img/bestWay-man.jpg" },
+    { src: "/img/cat.png" },
+    { src: "/img/crying-cat.jpg" },
+    { src: "/img/denying man.PNG" },
+    { src: "/img/Parviz.jpeg" },
+  ];
+  const cover = { src: "/img/cover.jpg" };
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...images, ...images]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Meme Memory</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards} className="btn">
+        New Game
+      </button>
     </div>
   );
 }
